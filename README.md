c6a3472c13d78446e061bd7fda3c7f16d1dcd3e3
Sergei Stadnik <sergeisqa@gmail.com>
2024-06-11 12:29:45 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aws_bedrock
 `pip install -r requiremets.txt`
+# if problem with boto3 installation
+`pip install boto3 -t .`
---
+++ app.py
@@ -0,0 +1,20 @@
+import json
+import os
+import sys
+import boto3
+#import streamlit as st
+
+## We will be suing Titan Embeddings Model To generate Embedding
+
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain.llms.bedrock import Bedrock
+
+## Data Ingestion
+
+import numpy as np
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
+# Vector Embedding And Vector Store
+
+from langchain.vectorstores import FAISS
